7b9a2cd742922d48a001b497f20d74fe06132a60
Curtis Webb <curtwebb1194@@gmail.com>
2023-06-07 13:30:40 -0500

    Most Secure Scores

--- Program.cs
+++ Program.cs
@@ -56,6 +56,7 @@ namespace Heist2Group
                 while (String.IsNullOrEmpty(newName))
                 {
                     Console.Write("Try again... nerd: ");
+                    newName = Console.ReadLine();
                 }
                 Console.Write(@$"What is {newName}'s specialty? Select the corresponding number.
                 1) Hacker
@@ -70,25 +71,99 @@ namespace Heist2Group
                 2) Muscles
                 3) Lock Specialist
                 :");
+                    specialtyNumber = Console.ReadLine();
                 }
                 Console.Write($"What is {newName}'s skill level? Select a number between 0 and 100: ");
                 string skillLevel = Console.ReadLine(); //needs to be parsed to int on construction of object
                 while (String.IsNullOrEmpty(skillLevel))
                 {
                     Console.Write("Try again... : ");
+                    skillLevel = Console.ReadLine();
                 }
                 Console.Write($"What percentage cut is {newName} expecting? Select a number between 0 and 100: ");
                 string percCut = Console.ReadLine(); //needs to be parsed to int on construction of object
                 while (String.IsNullOrEmpty(percCut))
                 {
                     Console.Write("Really..? You're hopeless, but try again anyway: ");
+                    percCut = Console.ReadLine();
                 }
-                // switch(specialtyNumber)
-                // {
-                //     case "1"
-                // }
+                switch (specialtyNumber)
+                {
+                    case "1":
 
+                        return new Hacker()
+                        {
+                            Name = newName,
+                            SkillLevel = int.Parse(skillLevel),
+                            PercentageCut = int.Parse(percCut)
+
+
+                        };
+
+                    case "2":
+                        return new Muscles()
+                        {
+                            Name = newName,
+                            SkillLevel = int.Parse(skillLevel),
+                            PercentageCut = int.Parse(percCut)
+                        };
+
+                    case "3":
+                        return new LockSpecialist()
+                        {
+                            Name = newName,
+                            SkillLevel = int.Parse(skillLevel),
+                            PercentageCut = int.Parse(percCut)
+                        };
+
+                    default:
+                        Console.WriteLine("You Suck, We'll give you a crew member.");
+                        return new Muscles()
+                        {
+                            SkillLevel = new Random().Next(0, 101),
+                            PercentageCut = new Random().Next(0, 2) * 100,
+                            Name = newName
+                        };
+                }
             }
+            Bank randomBank = new Bank
+                {
+                    AlarmScore = new Random().Next(0,100),
+                    VaultScore = new Random().Next(0,100),
+                    SecurityGuardScore = new Random().Next(0,100),
+                    CashOnHand = new Random().Next(50000,1000000)
+                };
+            
+            Dictionary<string, int> scoreName = new Dictionary<string, int>()
+            {
+                {"Alarm Score",randomBank.AlarmScore},
+                {"Vault Score", randomBank.VaultScore},
+                {"Security Guard Score", randomBank.SecurityGuardScore}
+            };
+
+            int maxScore = Math.Max(randomBank.AlarmScore, Math.Max(randomBank.VaultScore, randomBank.SecurityGuardScore));
+            int minScore =  Math.Min(randomBank.AlarmScore, Math.Min(randomBank.VaultScore, randomBank.SecurityGuardScore));
+
+            string mostSecureScore = "";
+            string leastSecureScore = "";
+
+            foreach (var entry in scoreName)
+            {
+                if (entry.Value == maxScore)
+                {
+                    mostSecureScore = entry.Key;
+                }
+                if (entry.Value == minScore)
+                {
+                    leastSecureScore = entry.Key;
+                }
+            }
+
+            Console.WriteLine($"Most Secure Score: {mostSecureScore}");
+            Console.WriteLine($"Least Secure Score: {leastSecureScore}");
+            
+
+
         }
     }
 }
